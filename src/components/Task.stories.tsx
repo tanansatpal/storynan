a719297153f0581d84f72b8359be0c1413066c52
Satpal Tanan <tanansatpal@gmail.com>
2020-07-08 01:14:01 +0530

    Added Knobs

--- src/components/Task.stories.tsx
+++ src/components/Task.stories.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-
+import {action} from '@storybook/addon-actions';
+import {withKnobs, object} from '@storybook/addon-knobs';
 import Task from './Task';
 
 export default {
     component: Task,
     title: 'Task',
+    decorators: [withKnobs],
     // Our exports that end in "Data" are not stories.
     excludeStories: /.*Data$/,
 };
@@ -22,10 +23,16 @@ export const actionsData = {
     onArchiveTask: action('onArchiveTask'),
 };
 
-export const Default = () => <Task task={{ ...taskData }} {...actionsData} />;
+export const Default = () => <Task task={object('task', {...taskData})} {...actionsData} />;
 
-export const Pinned = () => <Task task={{ ...taskData, state: 'TASK_PINNED' }} {...actionsData} />;
+export const Pinned = () => <Task task={{...taskData, state: 'TASK_PINNED'}} {...actionsData} />;
 
 export const Archived = () => (
-    <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
+    <Task task={{...taskData, state: 'TASK_ARCHIVED'}} {...actionsData} />
+);
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = () => (
+    <Task task={{...taskData, title: longTitleString}} {...actionsData} />
 );
